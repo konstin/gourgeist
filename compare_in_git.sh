c9768d868f8545870bc98d3cd49274d912db368f
konstin <konstin@mailbox.org>
2023-09-24 18:58:08 +0200

    Improve compare in git

--- compare_in_git.sh
+++ compare_in_git.sh
@@ -2,17 +2,25 @@
 
 set -e
 
+virtualenv_command() {
+  virtualenv -p 3.11 compare_venv # --no-pip --no-setuptools --no-wheel
+}
+rust_command() {
+  cargo run -- -p 3.11 compare_venv # --bare
+}
+
 rm -rf compare_venv
-virtualenv -p 3.11 compare_venv
+virtualenv_command
 rm compare_venv/.gitignore
 git -C compare_venv init
 git -C compare_venv add -A
 git -C compare_venv commit -q -m "Initial commit"
 rm -r compare_venv/* # This skips the hidden .git
 mv compare_venv compare_venv2
-cargo run -- compare_venv
+rust_command
 rm compare_venv/.gitignore
 cp -r compare_venv/* compare_venv2
 rm -r compare_venv
 mv compare_venv2 compare_venv
 git -C compare_venv/ status
+
